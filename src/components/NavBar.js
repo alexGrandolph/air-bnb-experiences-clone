78352eb9eadcf3987145a147ec4be6e94c736f70
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-10 12:13:25 -0600

    feat: add NavBar compononent

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <img className="nav-logo" src="../assets/images/airbnb-logo.png" alt="logo"/>
+    </nav>
+  )
+}
+
+export default NavBar
