43b7bc5e70b024b4e6b910d31f93d9134818a8da
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-10 12:41:17 -0600

    feat: add stylings for hero container

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,13 @@ import photogrid from '../assets/images/photo-grid.png';
 function Hero() {
   return (
     <div className="hero-container">
-      <img className="hero-photo-grid" src={photogrid} alt="photo-grid"/>
-      <h1 className="hero-title">Online Experiences</h1>
-      <p className="hero-description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      <div className="hero-image-container">
+        <img className="hero-photo-grid" src={photogrid} alt="photo-grid"/>
+      </div>
+       <div className="hero-text-container">
+        <h1 className="hero-title">Online Experiences</h1>
+        <p className="hero-description">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      </div>
     </div>
   )
 }
