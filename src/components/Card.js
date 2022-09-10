d0d120373df24904da58e4cc0166fa83b29ab937
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-10 17:59:26 -0600

    feat: finish card stats for card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,14 @@ function Card() {
   return (
     <div className="card-container">
       <img className="card-image" src={KatieImage} alt="card-image" />
-      <img className="card-start" src={CardStar} alt="card-star" />
+      <div className="card-stats">
+        <img className="card-star" src={CardStar} alt="card-star" />
+        <span>5.0 </span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p><span className="bold">From $136 </span>/ person</p>
     </div>
   )
 }
