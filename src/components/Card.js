53461ce1f50ac0e4e9bfcb5ac13f6e2b7f32c981
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-10 19:45:54 -0600

    feat: add badge to each card dependent on available slots

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,31 +4,37 @@ import cardData from '../data/cards.json';
 function Card() {
 
 
-
-const renderCards = (cards) => {
-  return (
-    <div className = "cards-list">
-      {
-        cards.map((card) => {
-          return (
-          <div className="card-container">
-              <img className="card-image" src={`${card.image}`} alt="card-image" />
-            <div className="card-stats">
-              <img className="card-star" src={CardStar} alt="card-star" />
-              <span>{card.stars}</span>
-              <span className="gray">{card.ratings} • </span>
-              <span className="gray">{card.location}</span>
+  const renderCards = (cards) => {
+    return (
+      <div className = "cards-list">
+        {
+          cards.map((card, index) => {
+            let badgeText
+            if (card.openSpots == 0) {
+              badgeText = "SOLD OUT"
+            } else if (card.openSpots != 0)  {
+              badgeText = "AVAILABLE"
+            }
+            return (
+            <div className="card-container">
+                {badgeText && <div className="card--badge" key={index}>{badgeText}</div>}
+                <img className="card-image" src={`${card.image}`} alt="card-image" />
+              <div className="card-stats">
+                <img className="card-star" src={CardStar} alt="card-star" />
+                <span>{card.stars}</span>
+                <span className="gray">{card.ratings} • </span>
+                <span className="gray">{card.location}</span>
+              </div>
+              <p>{card.description}</p>
+              <p><span className="bold">{card.price} </span>/ {card.perPerson}</p>
             </div>
-            <p>{card.description}</p>
-            <p><span className="bold">{card.price} </span>/ {card.perPerson}</p>
-          </div>
-          )
-        })
-      }
+            )
+          })
+        }
 
-    </div>
-  );
-    }
+      </div>
+    );
+  }
   return (
     <div>
       <div>{renderCards(cardData.cards)}</div>
